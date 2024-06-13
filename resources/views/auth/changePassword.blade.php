c2bf8bec82d0103ef83bdbc79eb5b606a9196e5f
MyatPwintMpp <myatpwintphyu@cgm-myanmar.com>
2024-06-13 09:28:47 +0630

    Authentication_review_change

--- resources/views/auth/changePassword.blade.php
+++ resources/views/auth/changePassword.blade.php
@@ -7,24 +7,32 @@
                 <h3 class="text-center mb-5">Change Password</h3>
             </div>
             <div><input type="hidden" value="{{ $user->id }}"></div>
-            <div class="text-danger text-center">
-                @if (count($errors) > 0)
-                    @foreach ($errors->all() as $error)
-                        <p>{{ $error }}</p>
-                    @endforeach
-                @endif
-            </div>
             <div class="form-group p-2">
                 <label for="current_password">Current Password</label><br>
                 <input type="password" id="current_password" name="current_password" class="form-control">
+                @if ($errors->has('current_password'))
+                    <div class="text-danger">
+                        <p>{{ $errors->first('current_password') }}</p>
+                    </div>
+                @endif
             </div>
             <div class="form-group p-2">
                 <label for="new_password">New Password</label><br>
                 <input type="password" id="new_password" name="new_password" class="form-control">
+                @if ($errors->has('new_password'))
+                    <div class="text-danger">
+                        <p>{{ $errors->first('new_password') }}</p>
+                    </div>
+                @endif
             </div>
             <div class="form-group p-2">
                 <label for="old_password">Confirm New Password</label><br>
                 <input type="password" id="new_password_confirmation" name="new_password_confirmation" class="form-control">
+                @if ($errors->has('new_password_confirmation'))
+                    <div class="text-danger">
+                        <p>{{ $errors->first('new_password_confirmation') }}</p>
+                    </div>
+                @endif
             </div>
             <div class="form-submit-btn">
                 <button type="submit" class="btn btn-primary btn-lg">Submit</button>
