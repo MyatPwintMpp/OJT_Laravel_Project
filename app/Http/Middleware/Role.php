0797f6de833d8eb1ccf2863dc0d85bfb5ffc3f6c
MyatPwintMpp <myatpwintphyu@cgm-myanmar.com>
2024-06-14 10:01:21 +0630

    Middleware Review Change

--- app/Http/Middleware/Role.php
+++ app/Http/Middleware/Role.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 
 class Role
 {
@@ -20,13 +23,13 @@ class Role
             return redirect()->route('loginScreen');
         } else {
             $user = Auth::user();
-            if ($user->role=='1') {
+            if ($user->role == User::ROLE_ADMIN) {
                 return $next($request);
             } else {
                 if ($user->id == $request->id) {
                     return $next($request);
                 } else {
-                    return back()->withErrors('You can only modify your own profile.');
+                    return redirect()->back()->withErrors(Config::get('constants.error_messages.modify_own_profile'))->withInput();
                 }
             }
         }
---
+++ config/constants.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'error_messages' => [
+        'modify_own_profile' => 'You can only modify your own profile.',
+    ],
+];
